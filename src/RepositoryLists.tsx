fa9b751d86609a6ce5a02fab27ce01d05f88fae5
noeline <noeline.mar@onja.org>
2022-06-01 17:16:34 +0300

    adding flexible search

--- src/RepositoryLists.tsx
+++ src/RepositoryLists.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,9 +6,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+
+import LoadingButton from '@mui/lab/LoadingButton';
 
 import { useQuery } from 'graphql-hooks';
+import { Search } from '@mui/icons-material';
 
 type data = {
   nameWithOwner: string;
@@ -18,8 +24,8 @@ type data = {
   id: string;
 };
 
-const REPOSITORIES_QUERY = `query Repository($limit: Int) {
-  search(query:"react", type:REPOSITORY, first:$limit ){
+const REPOSITORIES_QUERY = `query Repository($limit: Int, $searchTerm: String!) {
+  search(query:$searchTerm, type:REPOSITORY, first:$limit ){
     repositoryCount,
 		nodes {
 			... on Repository{
@@ -34,16 +40,59 @@ const REPOSITORIES_QUERY = `query Repository($limit: Int) {
 }
 `;
 
+const DefaultSearchTerm = 'react';
+const Limit = 10
+
 export default function RepositoryLists() {
-  const { loading, error, data } = useQuery(REPOSITORIES_QUERY, {
+  const [searchTerm, setSearchTerm] = useState(DefaultSearchTerm);
+
+  const { loading, error, data, refetch } = useQuery(REPOSITORIES_QUERY, {
     variables: {
-      limit: 10,
-      searchTerm: 'react',
+      limit: Limit,
+      searchTerm: DefaultSearchTerm,
     },
   });
 
+  const handleSearch = () => {
+    refetch({
+      variables: {
+        limit: Limit,
+        searchTerm,
+      },
+    });
+  };
+
   return (
     <>
+    <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: "space-between",
+          p: 1,
+          m: 1,
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+        }}
+      >
+      <TextField
+        id='repository'
+        label='Repository name'
+        variant='outlined'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <LoadingButton
+        loading={loading}
+        loadingPosition='start'
+        startIcon={<Search />}
+        variant='outlined'
+        onClick={handleSearch}
+      >
+        Search
+      </LoadingButton>
+      </Box>
       {error && <div>{JSON.stringify(error)}</div>}
       <div>
         {loading ? (
